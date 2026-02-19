92e792412df9774a7b44a8ebd429ab8650ca6ffe
Parth <feargeralt5@gmail.com>
2026-02-19 23:33:37 +0530

    v8

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 7 Running 🚀🔥"
+    return "Version 8 Running 🚀🔥"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
