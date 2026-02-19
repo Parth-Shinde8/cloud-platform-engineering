b148e0842283e13a424355517b4d1fbb63549680
Parth <feargeralt5@gmail.com>
2026-02-19 16:10:02 +0530

    Version 2 update

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Cloud Platform Engineering Project Running 🚀"
+    return "Version 2 Running 🚀🔥"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -16,6 +16,14 @@ resource "aws_instance" "k8s_master" {
   vpc_security_group_ids = [var.security_group_id]
   key_name               = var.key_name
 
+  associate_public_ip_address = true
+
+  
+  root_block_device {
+    volume_size = 20      # 20GB (within 30GB free tier)
+    volume_type = "gp3"
+  }
+
   tags = {
     Name = "${var.environment}-k8s-master"
   }
